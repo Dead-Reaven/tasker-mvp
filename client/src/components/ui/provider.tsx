9b7960c8fd2e55ab4d8c29e997b0659af26b572d
Illia <ilyusha.drozd.2016@gmail.com>
2025-07-07 16:26:21 +0300

    test bild

--- client/src/components/ui/provider.tsx
+++ client/src/components/ui/provider.tsx
@@ -2,7 +2,7 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { ColorModeProvider, type ColorModeProviderProps } from './color-mode'
 import { theme } from '../../../theme'
 
-export function Provider(props: ColorModeProviderProps) {
+export default function Provider(props: ColorModeProviderProps) {
 	return (
 		<ChakraProvider value={theme}>
 			<ColorModeProvider {...props} />
